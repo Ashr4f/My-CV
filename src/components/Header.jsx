b00d150b90dda479fc7a0e0dcd04a802d222b631
Ashraf <ashraf@post.com>
2019-10-24 14:42:23 +0200

    Added font and index.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import { Link, Router } from "react-router-dom";
+const createBrowserHistory = require("history").createBrowserHistory;
+const history = createBrowserHistory();
 
 const Header = () => {
   return (
     <div className={"header-container"}>
-      <header className={"header"}>
-        <h1>{"Header"}</h1>
-      </header>
+      <div className="container">
+        <header className={"header"}>
+          <Router history={history}>
+            <div className="header-logo">
+              <Link to={"/"}>{"Ashraf"}</Link>
+            </div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to={"/"}>{"Home"}</Link>
+                </li>
+
+                <li>
+                  <Link to={"/about"}>{"About"}</Link>
+                </li>
+
+                <li>
+                  <Link to={"/service"}>{"Service"}</Link>
+                </li>
+
+                <li>
+                  <Link to={"/work"}>{"Work"}</Link>
+                </li>
+
+                <li>
+                  <Link to={"/resume"}>{"Resume"}</Link>
+                </li>
+
+                <li>
+                  <Link to={"/contact"}>{"Contact"}</Link>
+                </li>
+              </ul>
+            </nav>
+          </Router>
+        </header>
+      </div>
     </div>
   );
 };
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+const Index = () => {
+  return (
+    <div className={"index"}>
+      <p>{"Index"}</p>
+    </div>
+  );
+};
